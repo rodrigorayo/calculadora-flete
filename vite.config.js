c92b447cf4ff319354be8289580ae819507c7f12
rodrigorayo <rodrigorayomartinez@gmail.com>
2025-12-30 10:42:14 -0400

    deploy configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/calculadora-flete/', // IMPORTANTE: Pon el nombre de tu repo entre barras
 })
